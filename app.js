2963fe0663dc32b6c4e5cdddf8e38e62b8e2d3e2
Ganesh <ganeshsindagi7@gmail.com>
2021-09-11 20:39:28 +0530

    Register and login implemented

--- app.js
+++ app.js
@@ -19,7 +19,52 @@ var connection = mysql.createConnection({
 connection.connect();
 
 app.get("/", (req, res) => {
-  res.render("home.ejs");
+  res.render("home");
+})
+
+app.get("/register", (req, res) => {
+  res.render("register");
+})
+
+app.post("/register", (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  connection.query('INSERT INTO user (name, email, password) VALUES (?, ?, ?)', [name, email, password], function (error, results, fields) {
+    if (error){
+        console.log(error);
+    } else {
+        // res.render('login')
+    }
+});
+    
+
+
+})
+
+app.get("/login", (req, res) => {
+  res.render("login");
+})
+
+app.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  connection.query('SELECT user_id, password, name FROM user WHERE email = ?',[email], function (error, results) {
+    if (error){
+        res.send("<h1>No User exist</h1>")
+        console.log(error);
+    } else {
+        if(results[0].password === password) {
+            // res.render('studentdetails', {username: results[0].username, user_id: results[0].user_id})
+            console.log("success");
+            console.log(results[0])
+        } else {
+            console.log("password didn't match")
+        }
+    }
+  });
 })
 
 app.listen(port, () => {
